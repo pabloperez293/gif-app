fc1aa4028fc12a96d340e86e9e5c5a9f32d2167e
pabloperez293 <paperezpablo@gmail.com>
2023-03-21 16:46:40 -0300

    Creacion de entorno gif

---
+++ src/gifExpert.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// desde la version 17 no hace falta que importemos react
+
+export const GifExpert = () => {
+  return (
+    <>
+    {/* titulo */}
+    <div>GifExpert</div>
+
+    {/* input */}
+
+    {/* Listado de tarjetas */}
+
+        {/* Item tarjetas */}
+    </>
+  )
+}
