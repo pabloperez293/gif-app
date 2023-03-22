5679f7b226bfc758e6db6e9513b1b844dbe1d3d5
pabloperez293 <paperezpablo@gmail.com>
2023-03-21 23:47:58 -0300

    Agregar element a un array

--- src/gifExpert.jsx
+++ src/gifExpert.jsx
@@ -1,15 +1,37 @@
 // import React from 'react'
 // desde la version 17 no hace falta que importemos react
+import { useState } from 'react';
 
 export const GifExpert = () => {
+
+    // Esto se almacena y se inicializa en un arreglo 
+    const [ categories, setCategories ] = useState([ " Spider-man" , "Venom" ]);
+
+    // console.log(categories);
+
+    const onAddCategory = () =>{
+        // console.log("marvel")
+        // setCategories([...categories,"iron-man"]); para agregar elemnt a un array
+        setCategories( cat => [ ...cat, "Cap"])
+    }
+
   return (
     <>
     {/* titulo */}
-    <div>GifExpert</div>
+    <h2>GifExpert</h2>
 
     {/* input */}
 
     {/* Listado de tarjetas */}
+    <button onClick={ onAddCategory }> Agregar </button>
+
+    <ol>
+    {/* con el arreglo de linea 8 , hace un conteo colocandolos en el html */}
+        { categories.map( categories => { 
+            return <li key={ categories }> { categories } </li>
+        }) }
+
+    </ol>
 
         {/* Item tarjetas */}
     </>
